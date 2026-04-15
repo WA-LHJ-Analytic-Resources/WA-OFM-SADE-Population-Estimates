221833231a41c2bd2458a1886c6619d7427049ad
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-04-15 14:55:56 -0700

    Implement upload to DuckDB from CSV file

--- Scripts/1_pull_data_alt.R
+++ Scripts/1_pull_data_alt.R
@@ -3,37 +3,41 @@
 # Establish DuckDB Connection -----
 con <- dbConnect(duckdb::duckdb(), params$duckdb_filepath)
 
+# Create Ingestion Timestamp ----
+ingest_ts <- lubridate::now(tzone = "America/Los_Angeles")
+
 # Load WA OFM Geographic Crosswalk File into DuckDB -----
-## read_auto_csv() streams the data from the .csv file into a new DuckDB table.
 
-filepath <- ""
+## Upload CSV file to DuckDB
+duckdb::duckdb_read_csv(
+  con,
+  name = "GEOGRAPHIC_CROSSWALK_RAW",
+  files = params$ofm_geo_crosswalk_filepath,
+  nrow.check = -1,
+  lower.case.names = TRUE
+)
 
-sql <- glue(
-  "
-  CREATE TABLE GEOGRAPHIC_CROSSWALK AS
-  SELECT 
-    *,
-    current_timestamp AS ingestion_ts
-  FROM read_csv_auto('{filepath}', HEADER=TRUE);
-"
-) # current_timestamp logs the date-time the WA OFM SADE data was uploaded to DuckDB
+## Perform Light Edits on DuckDB Table
+tbl(con, "GEOGRAPHIC_CROSSWALK_RAW") %>%
+  mutate(
+    across(everything(), as.character), # Cast all columns to character
+    ingestion_ts = !!ingest_ts # A single constant timestamp for all rows
+  ) %>%
+  compute(
+    name = "GEOGRAPHIC_CROSSWALK", # Save cleaned table as GEOGRAPHIC_CROSSWALK
+    temporary = FALSE,
+    overwrite = TRUE
+  )
 
-DBI::dbExecute(con, sql)
-
+## Remove GEOGRAPHIC_CROSSWALK_RAW
+dbRemoveTable(con, "GEOGRAPHIC_CROSSWALK_RAW")
 
 # Load Small Area Demographic Estimates File into DuckDB -----
-## read_auto_csv() streams the data from the .csv file into a new DuckDB table.
 
-filepath <- ""
-
-sql <- glue(
-  "
-  CREATE TABLE RAW_UPLOAD_NEW AS
-  SELECT 
-    *,
-    current_timestamp AS ingestion_ts
-  FROM read_csv_auto('{filepath}', HEADER=TRUE);
-"
-) # current_timestamp logs the date-time the WA OFM SADE data was uploaded to DuckDB
-
-DBI::dbExecute(con, sql)
+## Upload CSV to DuckDB
+duckdb::duckdb_read_csv(
+  con,
+  "RAW_UPLOAD",
+  params$sade_filepath,
+  lower.case.names = TRUE # Converts all column names to lowercase
+)
