661d7bb1411ec8b9f5962fa7916242d14e31d324
Tyler Bonnell <Tyler.Bonnell@co.snohomish.wa.us>
2026-04-15 14:55:43 -0700

    Remove unneeded API code

--- Scripts/0_setup.R
+++ Scripts/0_setup.R
@@ -4,15 +4,13 @@
 
 ## Install/Load R Packages
 pacman::p_load(
-  base64enc,
   DBI,
   duckdb,
   glue,
   here,
-  keyring,
+  lubridate,
   tictoc,
   tidyverse,
-  tigris,
   writexl
 )
 
@@ -28,9 +26,19 @@ list.files(
 # Define Parameters -----
 params <- list()
 
-## On/Off Switch to Pull New Data via API (activates 1_pull_data.R)
-params$pull_new_data <- TRUE
+## Define Output Folder (for WA OFM SADE estimates)
 params$output_folder <- Sys.getenv("OUTPUT_FILEPATH")
+params$duckdb_filepath <- here(
+  params$output_folder,
+  "WA_OFM_SADE_Population_Data.duckdb"
+)
+
+## Define Filepaths for WA OFM Geographic Crosswalk & Small Area Demographic Estimate Exports
+
+### Note: Define these filepaths in the .Renviron file (this ensures the filepath is protected and not upload to GitHub!)
+params$ofm_geo_crosswalk_filepath <- Sys.getenv("OFM_GEO_CROSSWALK_FILEPATH") # Download at (quick): https://data.wa.gov/demographics/OFM-Geographic-Crosswalk/pvty-6zcu/about_data
+params$sade_filepath <- Sys.getenv("SADE_FILEPATH") # Download at (takes ~40 minutes): https://data.wa.gov/en/demographics/Small-Area-Demographic-Estimates-2020-present/3s8k-fvmm/about_data
+
 
 ## Define county_of_interest (for formatting data extracts in 3_use_data.R)
 params$county_of_interest <- "Snohomish" # EDIT as needed. Do not include "County"
@@ -59,60 +67,8 @@ params$age_labels <- c(
 
 params$age_breaks <- convert_age_labels_to_breaks(params$age_labels)
 
-# Load API Credentials ------
-params$username <- keyring::key_get("DATA_WA_GOV_USERNAME")
-params$password <- keyring::key_get("DATA_WA_GOV_PASSWORD")
-params$app_token <- keyring::key_get("WA_OFM_APP_TOKEN")
-params$basic_b64 <- base64enc::base64encode(charToRaw(paste0(
-  params$username,
-  ":",
-  params$password
-))) # Convert data.wa.gov password into base64 encoded raw bytes (the format that Socrata/data.wa.gov expects authentication credentials in)
-
-# Setup DuckDB -----
+# Connect to DuckDB -----
+con <- dbConnect(duckdb::duckdb(), params$duckdb_filepath)
 
 ## Note: DuckDB will be the preferred file format to store the pulled data at it is uniquely tailored to handle large data sets in an efficient, simple, and streamlined way.
 ## DuckDB Resource: https://borkar.substack.com/p/r-workflows-with-duckdb
-
-## Create DuckDB filepath
-params$duckdb_filepath <- here(
-  params$output_folder,
-  "WA_OFM_SADE_Population_Data.duckdb"
-)
-
-## Create DuckDB connection
-con <- dbConnect(duckdb::duckdb(), params$duckdb_filepath)
-
-## Install & Loadd HTTPFS extension to enable streaming
-dbExecute(con, "INSTALL httpfs;")
-dbExecute(con, "LOAD httpfs;")
-
-## Add Temporary Secrets (API Credentials) to DuckDB to enable Streaming
-dbExecute(
-  con,
-  glue(
-    "
-  CREATE OR REPLACE SECRET socrata_http (
-    TYPE http,
-    EXTRA_HTTP_HEADERS MAP {{
-      'X-App-Token': '{params$app_token}',
-      'Authorization': 'Basic {params$basic_b64}'
-    }}
-  );
-"
-  ) # X-App-Token is the data.wa.gov App Token
-)
-
-# Add WA OFM Geographic Crosswalk DuckDB -----
-geo_cw <- RSocrata::read.socrata(
-  url = "https://data.wa.gov/resource/pvty-6zcu.csv", # Source: https://data.wa.gov/demographics/OFM-Geographic-Crosswalk/pvty-6zcu/about_data
-  app_token = params$app_token
-) %>%
-  # Convert all variables to character data type
-  mutate(across(everything(), ~ as.character(.x)))
-
-## Upload fips_crosswalk to DuckDB
-DBI::dbWriteTable(con, "GEOGRAPHIC_CROSSWALK", geo_cw, overwrite = TRUE)
-
-# rm(geo_cw)
-# gc()
